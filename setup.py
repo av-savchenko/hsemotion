7707c53d4e56adba2d7e8b8ed50b1df0adb67e75
avsavchenko <andrey.v.savchenko@gmail.com>
2022-12-17 12:54:27 +0300

    Code of the Python package is copied from the main repository face-emotion-recognition

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ requirements = [
 ]
 setup(
     name='hsemotion',
-    version='0.2.1',
+    version='0.3.0',
     license='Apache-2.0',
     author="Andrey Savchenko",
     author_email='andrey.v.savchenko@gmail.com',
     packages=find_packages('.'),
-    download_url = 'https://github.com/HSE-asavchenko/face-emotion-recognition/archive/v0.2.1.tar.gz',
+    download_url = 'https://github.com/HSE-asavchenko/hsemotion/archive/v0.3.0.tar.gz',
     url='https://github.com/HSE-asavchenko/face-emotion-recognition',
     description='HSEmotion Python Library for Facial Emotion Recognition',
-    keywords='face emotion recognition',
+    keywords=['face expression recognition', 'emotion analysis', 'facial expressions'],
     install_requires=requirements,
 )
